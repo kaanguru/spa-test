b6f2eda2c84129b359ee560f8fa99af97a5bd7ef
kaanguru <kaanguru@mail.ru>
2024-02-10 20:42:45 +0300

    update 2 files and delete 1 file

--- src/routes/+page.server.ts
+++
@@ -1,4 +0,0 @@
-import type { Actions } from './$types';
-export const actions = { default: async () => {
-
-} } satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,50 @@
 <script lang="ts">
 	import { setMessage, superForm, superValidateSync } from 'sveltekit-superforms/client';
 	import { _userSchema } from '$lib/db';
-	import * as Form from "$lib/components/ui/form";
-	export let form = superForm(superValidateSync(_userSchema), {
-		SPA: true,
-		validators: _userSchema,
-		onUpdate({ form }) {
-			if (form.valid) {
-				console.log('Form is valid!');
-                setMessage(form, 'Valid data!');
-				console.log(form.data);
-			} else {
-				console.log('Form is invalid!');
-				console.log(form.data);
-				console.log(form);
+  import { Input } from "$lib/components/ui/input";
+  import { Label } from "$lib/components/ui/label";
+  import { Button } from "$lib/components/ui/button";
+	const { form, errors, enhance, message, constraints } = superForm(
+		superValidateSync(_userSchema),
+		{
+			SPA: true,
+			validators: _userSchema,
+			onUpdate({ form }) {
+				if (form.valid) {
+					console.log('Form is valid!');
+					setMessage(form, 'Valid data!');
+					console.log(form.data);
+				} else {
+					console.log('Form is invalid!');
+					console.log(form.data);
+					console.log(form);
+				}
 			}
 		}
-	});
-
+	);
 </script>
+
 <h1>Kullanıcı Ekleme</h1>
-<Form.Root  {form} schema={_userSchema}  let:config>
-  <Form.Field {config} name="name">
-    <Form.Item>
-      <Form.Label>Name</Form.Label>  
-      <Form.Input />
-      <Form.Description>This is your public display name.</Form.Description>
-      <Form.Validation />
-    </Form.Item>
-  </Form.Field>
+<form method="POST" use:enhance>
+  <div class="flex flex-col w-full max-w-sm gap-1.5">
+	{#if $message}<p>{$message}</p>{/if}
+	<Label for="name">Name</Label>
+		<Input
+			aria-invalid={$errors.name ? 'true' : undefined}
+			bind:value={$form.name}
+			{...$constraints.name}
+		/>
+	{#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
+</div>
 
-  <Form.Field {config} name="email">
-    <Form.Item>
-      <Form.Label>Email</Form.Label>
-      <Form.Input />
-      <Form.Description>This is your email</Form.Description>
-      <Form.Validation />
-    </Form.Item>
-  </Form.Field>
+  <div class="flex flex-col w-full max-w-sm gap-1.5">
+    <Label for="email">Email</Label>
+    <Input type="email" id="email" placeholder="email" aria-invalid={$errors.email ? 'true' : undefined}
+    bind:value={$form.email}
+    {...$constraints.email} />
+    {#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
+  </div>
 
-  <Form.Button>Submit</Form.Button>
-</Form.Root>
+
+	<Button>Submit</Button>
+</form>
