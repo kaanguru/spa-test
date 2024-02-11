5ff06717e8d14e0857491a28cd1c78a0c6145e37
kaanguru <kaanguru@mail.ru>
2024-02-11 17:27:17 +0300

    update package.json, pnpm-lock.yaml, db.ts and +page.svelte

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,26 @@
 import { z } from 'zod';
-
+import Dexie, { type Table } from 'dexie';
 export const _userSchema = z.object({
-  name: z.string().min(2, { message: "BİRAZ DAHA UZUN YAP" }),
-  email: z.string().email()
+	name: z.string().min(2, { message: 'BİRAZ DAHA UZUN YAP' }),
+	email: z.string().email()
 });
+export interface User {
+	id?: number;
+	name: string;
+	email?: string | null;
+}
+export class LockITDatabase extends Dexie {
+	users: Table<User>;
+
+	constructor() {
+		super('LockITDatabase');
+
+		this.version(1).stores({
+			users: '++id, name , &email'
+		});
+
+		this.users = this.table('users');
+	}
+}
+
+export const db = new LockITDatabase();
