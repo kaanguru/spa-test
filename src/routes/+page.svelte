49f693f79d6a725456393f9e266dd5d8184de612
kaanguru <kaanguru@mail.ru>
2024-02-11 14:38:32 +0300

    update 7 files and create 4 files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,49 +2,50 @@
 	import { setMessage, superForm, superValidateSync } from 'sveltekit-superforms/client';
 	import { _userSchema } from '$lib/db';
 
-	const { form, errors, constraints, enhance , message} = superForm(superValidateSync(_userSchema), {
-		SPA: true,
-		validators: _userSchema,
-		onUpdate({ form }) {
-			if (form.valid) {
-				console.log('Form is valid!');
-                setMessage(form, 'Valid data!');
-				console.log(form.data);
-			} else {
-				console.log('Form is invalid!');
+	const { form, errors, constraints, enhance, message } = superForm(
+		superValidateSync(_userSchema),
+		{
+			SPA: true,
+			validators: _userSchema,
+			onUpdate({ form }) {
+				if (form.valid) {
+					console.log('Form is valid!');
+					setMessage(form, 'Valid data!');
+					console.log(form.data);
+				} else {
+					console.log('Form is invalid!');
+				}
 			}
 		}
-	});
+	);
 </script>
-<h1>Kullanıcı Ekleme</h1>
-<form method="POST" use:enhance>
-    {#if $message}<p>{$message}</p>{/if}
-	<label>
-		Name<br />
-		<input
-			aria-invalid={$errors.name ? 'true' : undefined}
-			bind:value={$form.name}
-			{...$constraints.name}
-		/>
-	</label>
-	{#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
 
-	<label>
-		E-mail<br />
-		<input
-			type="email"
-			aria-invalid={$errors.email ? 'true' : undefined}
-			bind:value={$form.email}
-			{...$constraints.email}
-		/>
-	</label>
-	{#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
+<div class="container">
+	<h1 class="h1">Kullanıcı Ekleme</h1>
+	<form method="POST" use:enhance>
+		{#if $message}<p class="input-success">{$message}</p>{/if}
+		<label >
+			<span>Name</span>
+			<input
+				aria-invalid={$errors.name ? 'true' : undefined}
+				bind:value={$form.name}
+				{...$constraints.name}
+			/>
+		</label>
+		{#if $errors.name}<span class="input-error">{$errors.name}</span>{/if}
 
-	<button>Submit</button>
-</form>
+		<label >
+			<span>E-mail</span>
+			<input
+				type="email"
+				aria-invalid={$errors.email ? 'true' : undefined}
+				bind:value={$form.email}
+				{...$constraints.email}
+			/>
+			{#if $errors.email}<span class="input-error">{$errors.email}</span>{/if}
+		</label>
 
-<style>
-	.invalid {
-		color: red;
-	}
-</style>
+		<button >Submit</button>
+	</form>
+</div>
+
