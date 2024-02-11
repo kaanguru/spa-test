866dc88a04c4e7d5b5dfe97f24658b2c2534bc9d
kaanguru <kaanguru@mail.ru>
2024-02-11 22:31:32 +0300

    update package.json, pnpm-lock.yaml and +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,43 @@
 <script lang="ts">
-	import AddUserForm from '../components/AddUserForm.svelte';
+	import { Formly, type IField } from 'svelte-formly';
+
+	const form_name = 'formly_alter';
+	const fields: IField[] = [
+		{
+			type: 'input',
+			name: 'firstname',
+			rules: ['required'],
+			attributes: {
+				type: 'text',
+				id: 'firstname',
+				placeholder: 'Tap your first name'
+			}
+		},
+		{
+			type: 'input',
+			name: 'password',
+			attributes: {
+				type: 'password',
+				id: 'password',
+				placeholder: 'Tap your password'
+			}
+		}
+	];
+	const onSubmit = (data: any) => {
+		console.log(data);
+	};
 </script>
 
-<div class="container">
-	<h1>Kullanıcı Ekleme</h1>
-	<AddUserForm />
-</div>
+<Formly
+	{form_name}
+	{fields}
+	on:submit={onSubmit}
+	btnSubmit={{
+		text: 'Submit alter',
+		classes: ['btn', 'btn-primary']
+	}}
+	btnReset={{
+		text: 'Reset alter',
+		classes: ['btn', 'btn-secondary']
+	}}
+/>
